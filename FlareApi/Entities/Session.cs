f46685106c963feb5c2e9bd69c5663a25a6b1e8f
Yury Rivera <shulkmaster@gmail.com>
2021-11-12 17:54:24 -0600

    :sparkles: JsonDateConverter.cs

---
+++ FlareApi/Entities/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlareApi.Entities
+{
+    [Table(nameof(Session))]
+    public class Session
+    {
+        public Guid Id { get; set; }
+        
+        public DateTime Expiration { get; set; }
+        
+        [ForeignKey(nameof(User.Uen))]
+        public string Uen { get; set; } = string.Empty;
+
+        public User UserNav { get; set; } = null!;
+    }
+}
