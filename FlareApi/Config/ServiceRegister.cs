22938a39ac42cbcfcac6656023ba574d32478db0
Yury Rivera <shulkmaster@gmail.com>
2021-11-12 01:08:07 -0600

    :construction: dinamic settings loader

---
+++ FlareApi/Config/ServiceRegister.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Text.Json.Serialization;
+using FlareApi.Api;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlareApi.Config
+{
+    public static class ServiceRegister
+    {
+        public static void AddServiceLayer(this IServiceCollection services, Settings settings)
+        {
+            services.AddSingleton(settings);
+            services.AddControllers()
+                .AddFluentValidation(validation => validation.RegisterValidatorsFromAssembly(typeof(Startup).Assembly))
+                .AddJsonOptions(options =>
+                {
+                    //options.JsonSerializerOptions.Converters.Add(new JsonDateConverter());
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                })
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.InvalidModelStateResponseFactory = context =>
+                    {
+                        var errors = context.ModelState.Values
+                            .Where(m => m.Errors.Count > 0)
+                            .SelectMany(m => m.Errors);
+                        var list = errors.Select(error =>
+                        {
+                            var innerError = error.Exception;
+                            ApiError? cause = null;
+                            if (innerError is not null)
+                            {
+                                cause = new ApiError(innerError.Source ?? "error", innerError.Message);
+                            }
+
+                            return new ApiError("Error", error.ErrorMessage, cause);
+                        });
+                        return new BadRequestObjectResult(list);
+                    };
+                });
+            //services.AddScoped<ISessionService, SessionService>();
+            //services.AddScoped<ITokenService, TokenService>();
+            services.AddAutoMapper(typeof(Startup));
+        }
+    }
+}
---
+++ FlareApi/Config/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace FlareApi.Config
+{
+    public enum DbProvider
+    {
+        SQLSERVER,
+        MySql
+    }
+
+    public class Settings
+    {
+        public DbProvider Provider { get; set; } = DbProvider.MySql;
+        public string ConnectionString { get; set; } = string.Empty;
+
+        private byte[] _tokeSecret = Array.Empty<byte>();
+
+        public string TokeSecret
+        {
+            set => _tokeSecret =Encoding.ASCII.GetBytes(value);
+        }
+
+        public long MaxFileBytes { get; set; } = 5_282_912L;
+
+        public byte[] getEncodeSecret()
+        {
+            return _tokeSecret;
+        }
+    }
+}
