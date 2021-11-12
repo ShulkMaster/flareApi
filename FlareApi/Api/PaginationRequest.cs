e6f409cffdf7c758b035aa21107852666ab3643f
Yury Rivera <shulkmaster@gmail.com>
2021-11-11 22:41:29 -0600

    :construction: adding basic response types

---
+++ FlareApi/Api/PaginationRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace FlareApi.Api
+{
+    public class PaginationRequest
+    {
+        public int Size { get; set; } = 20;
+        public int Page { get; set; } = 1;
+
+        public void Deconstruct(out int size, out int page)
+        {
+            size = Size;
+            page = Page;
+        }
+    }
+    
+    public class ZeroPageRequest: PaginationRequest { }
+}
