e9e7aa3c5e29e5472e444eab4814ec95bc46429c
Yury Rivera <shulkmaster@gmail.com>
2021-11-12 21:59:01 -0600

    :fire: ApiError.cs

--- FlareApi/Config/ServiceRegister.cs
+++ FlareApi/Config/ServiceRegister.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Linq;
 using System.Text.Json.Serialization;
-using FlareApi.Api;
+using FlareApi.Api.V1.DataAccess;
+using FlareApi.Repository;
 using FlareApi.Service;
 using FlareApi.Service.Driver;
 using Microsoft.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -24,27 +23,6 @@ namespace FlareApi.Config
                 {
                     options.JsonSerializerOptions.Converters.Add(new JsonDateConverter());
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                })
-                .ConfigureApiBehaviorOptions(options =>
-                {
-                    options.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context.ModelState.Values
-                            .Where(m => m.Errors.Count > 0)
-                            .SelectMany(m => m.Errors);
-                        var list = errors.Select(error =>
-                        {
-                            var innerError = error.Exception;
-                            ApiError? cause = null;
-                            if (innerError is not null)
-                            {
-                                cause = new ApiError(innerError.Source ?? "error", innerError.Message);
-                            }
-
-                            return new ApiError("Error", error.ErrorMessage, cause);
-                        });
-                        return new BadRequestObjectResult(list);
-                    };
                 });
             services.AddAuthentication(s =>
                 {
@@ -73,7 +51,8 @@ namespace FlareApi.Config
 
         public static void AddServiceLayer(this IServiceCollection services)
         {
-            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddTransient<ITokenService, TokenService>();
             services.AddAutoMapper(typeof(Startup));
         }
 
