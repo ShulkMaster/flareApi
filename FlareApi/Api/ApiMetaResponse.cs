ccb9ef36f598c37129d126813be50d6ba91ff85a
Yury Rivera <shulkmaster@gmail.com>
2021-11-12 20:32:18 -0600

    :fire: ApiResponse.cs

---
+++ FlareApi/Api/ApiMetaResponse.cs
@@ -0,0 +1,22 @@
+﻿using AutoWrapper.Wrappers;
+using System.Collections.Generic;
+
+namespace FlareApi.Api
+{
+    public class ApiMetaResponse<T> : ApiResponse
+    {
+        public Metadata Meta { get; }
+        public object? Filter { get; }
+
+        public ApiMetaResponse(
+            IEnumerable<T> value,
+            Metadata meta,
+            int code = 200,
+            object? filter = null
+        ) : base(value, code)
+        {
+            Meta = meta;
+            Filter = filter;
+        }
+    }
+}
