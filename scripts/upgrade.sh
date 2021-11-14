1a02706144060292ca0680ddfa6ccf29fa2998ef
Yury Rivera <shulkmaster@gmail.com>
2021-11-13 22:55:16 -0600

    :rocket: :fire: moving script remote inline

--- scripts/upgrade.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/bash
-echo "this is on the remote $GITHUB_WORKFLOW $GITHUB_RUN_ID" > file.txt
-echo "$REMOTE_USER"
-scp archive.tar.gz "$REMOTE_USER"@sovize.com:/var/www
-tar -xzvf /var/www/archive.tar.gz
