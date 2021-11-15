c7ea31fb5d30874c0de213bfa9a4e166f4986ee5
Yury Rivera <shulkmaster@gmail.com>
2021-11-15 16:29:28 -0600

    :heavy_plus_sign::card_file_box: adding migrations code

---
+++ FlareApi/Migrations/20211113050451_Init.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace FlareApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    AccessLevel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Uen = table.Column<string>(type: "varchar(12)", maxLength: 12, nullable: false),
+                    Name = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Password = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false),
+                    RoleId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    Active = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    ReactivationDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    FailedAttempts = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Uen);
+                    table.ForeignKey(
+                        name: "FK_User_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_User_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Session",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "CHAR(36)", nullable: false),
+                    Expiration = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Uen = table.Column<string>(type: "varchar(12)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Session", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Session_User_Uen",
+                        column: x => x.Uen,
+                        principalTable: "User",
+                        principalColumn: "Uen",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Session_Uen",
+                table: "Session",
+                column: "Uen");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DepartmentId",
+                table: "User",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleId",
+                table: "User",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Session");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
---
+++ FlareApi/Migrations/20211115220849_AgeGender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlareApi.Migrations
+{
+    public partial class AgeGender : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 18);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "User",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "User");
+        }
+    }
+}
