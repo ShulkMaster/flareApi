9990f6823d9ef3314a87905602c96bc2d7b3506f
Yury Rivera <shulkmaster@gmail.com>
2021-11-16 03:32:02 -0600

    :card_file_box: agregand migracion para la columna de fecha de ancimiento

---
+++ FlareApi/Migrations/20211116090743_birth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlareApi.Migrations
+{
+    public partial class birth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "User",
+                type: "datetime",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "User");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 18);
+        }
+    }
+}
